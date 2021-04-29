83232701a1253aa51dde73ab59a67e21005cfb02
RAJASEKARAN S <proffraja@gmail.com>
2021-04-29 21:33:31 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,5 +1,15 @@
 from main import star_print
+def test_star1():
+    actual=star_print(1)
+    expected="*"
+    assert actual == expected
+    
 def test_star2():
     actual=star_print(2)
     expected="**"
+    assert actual == expected
+    
+def test_star3():
+    actual=star_print(3)
+    expected="***"
     assert actual == expected
